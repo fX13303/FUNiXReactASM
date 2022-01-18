8bdc1418b2acd30f75347864e956e84f32cf2469
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-01-18 16:52:22 +0700

    second commit

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import dateFormat from "dateformat";
 
 function RenderStaff({staff}){
     if(staff != null){
@@ -12,8 +13,8 @@ function RenderStaff({staff}){
                         </div>
                         <div className="col-9">                    
                             <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                            <CardText>Ngày sinh: {staff.doB}</CardText>
-                            <CardText>Ngày vào công ty: {staff.startDate}</CardText>
+                            <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                            <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
                             <CardText>Phòng ban: {staff.department.name}</CardText>
                             <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                             <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
