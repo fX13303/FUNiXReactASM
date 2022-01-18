43288b47ff7360ba89455fbb32c13e31548caa0b
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-01-18 11:04:43 +0700

    first commit

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,70 +1,48 @@
 import React, {Component} from "react";
-import { Navbar, NavbarBrand, NavbarToggler,Collapse, NavItem, Jumbotron, Nav } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
-class Header extends Component {
 
-    constructor(props) {
+//Container Presentational Component Header
+class Header extends Component {
+    constructor(props){
         super(props);
         this.state = {
             isNavOpen: false
-        };
+        }
+
         this.toggleNav = this.toggleNav.bind(this);
     }
 
-    toggleNav() {
+    toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
-        });
+        })
     }
-    
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar dark expand='md'>
-                    <div className='container'>
+    render(){
+        return(
+            <div>
+                <Navbar dark expand="md" color="success" className="shadow mb-3">
+                    <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className='mr-auto' href='#'>
-                            <img src='assets/images/logo.png' height='30' width='41' alt='Restaurant Confusion' />
-                        </NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                                <NavItem>
-                                    <NavLink className='nav-link' to='/home'>
-                                        <span className="fa fa-home fa-lg"></span> Home
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className='nav-link' to='/aboutus'>
-                                        <span className="fa fa-info fa-lg"></span> About Us
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className='nav-link' to='/menu'>
-                                        <span className="fa fa-list fa-lg"></span> Menu
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className='nav-link' to='/contactus'>
-                                        <span className="fa fa-address-card fa-lg"></span> Contact Us
-                                    </NavLink>
-                                </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/staff'><span className="fa fa-users fa-lg"></span> Nhân viên</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to='/department'><span className="fa fa-id-card-o fa-lg"></span> Phòng Ban</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/salary'><span className="fa fa-money fa-lg"></span> Bảng Lương</NavLink>
+                            </NavItem>
                             </Nav>
-                            </Collapse>
+                        </Collapse>
                     </div>
                 </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>Restaurant Confusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lismacking creations will tickle your culinary senses!</p>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron>
-            </React.Fragment>
-        );
+            </div>
+        )
     }
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,55 +1,44 @@
 import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import Contact from './ContactComponent';
-import DishDetail from './DishDetailComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
+import StaffDetail from './StaffDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import StaffList from './StaffListComponent';
+import Department from './DepartmentComponent';
+import Salary from './SalaryComponent';
+import { STAFFS, DEPARTMENTS } from '../shared/staffs';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-class Main extends Component {
 
-  constructor(props) {
+class Main extends Component {
+  constructor(props){
     super(props);
-    this.state = {
-        dishes: DISHES,
-        comments: COMMENTS,
-        promotions: PROMOTIONS,
-        leaders: LEADERS
-    };
+    this.state = {      
+      staffs: STAFFS,
+      departments: DEPARTMENTS
+    }
   }
+
 
   render() {
 
-    const HomePage = () => {
+
+    const StaffWithId = ({match}) => {
       return(
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
-          promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
-          leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+        <StaffDetail
+          staff={this.state.staffs.filter((staff) => staff.id === parseInt(match.params.staffId,10))[0]}
         />
       );
     }
 
-    const DishWithId = ({match}) => {
-      return(
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-          />
-      );
-    }
     return (
       <div>
         <Header />
         <Switch>
-          <Route path='/home' component={HomePage} />
-          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-          <Route path='/menu/:dishId' component={DishWithId} />
-          <Route exact path='/contactus' component={Contact} />
-          <Redirect to='/home' />
+          <Route path="/staff/:staffId" component={StaffWithId} />
+          <Route path="/staff" component={() => <StaffList staffs={this.state.staffs} />} />
+          <Route path="/salary" component={() => <Salary staffs={this.state.staffs} />} />
+          <Route path="/department" component={() => <Department departments={this.state.departments} />} />
+          <Redirect to="/staff" />
         </Switch>
         <Footer />
       </div>
